db6c98461f46affae03abf5a3054bac51f9ee1c2
Kanchan Gobari <gobarikanchan53@gmail.com>
2024-03-18 21:51:31 +0530

    Added home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from "react-query";
+import * as apiClient from "../api-client";
+import LatestDestinationCard from  "../components/LatestDestinationCard";
+import { HotelType } from "../../../backend/src/shared/types";
+
+const Home = () => {
+  const { data: hotels } = useQuery("fetchQuery", () =>
+    apiClient.fetchHotels()
+  );
+
+  const topRowHotels = Array.isArray(hotels) ? hotels.slice(0, 2) : [];
+  const bottomRowHotels = Array.isArray(hotels) ? hotels.slice(2) : [];
+
+
+  return (
+    <div className="space-y-3">
+      <h2 className="text-3xl font-bold">Latest Destinations</h2>
+      <p>Most recent desinations added by our hosts</p>
+      <div className="grid gap-4">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
+          {topRowHotels.map((hotel: HotelType) => (
+            <LatestDestinationCard hotel={hotel} />
+          ))}
+        </div>
+        <div className="grid md:grid-cols-3 gap-4">
+          {bottomRowHotels.map((hotel: HotelType) => (
+            <LatestDestinationCard hotel={hotel} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
