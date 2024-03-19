e20ecf0bcd6da338d6b3d72d3c60aa872ae3f2a0
Kanchan Gobari <gobarikanchan53@gmail.com>
2024-03-19 21:30:59 +0530

    updated e2e test

--- e2e-test/tests/manage.hotels.spec.ts
+++ e2e-test/tests/manage.hotels.spec.ts
@@ -11,8 +11,8 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email]").fill("1@1.com");
-  await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=email]").fill("test1@gmail.com");
+  await page.locator("[name=password]").fill("123456");
 
   await page.getByRole("button", { name: "Login" }).click();
 
